2e9741bdd2fcf74679c56ee36dab8e60b9a6d441
Johan Åström <johan.astrom89@gmail.com>
2021-04-27 10:15:44 +0200

    Added RecipeInfo component

---
+++ src/components/RecipeInfo.vue
@@ -0,0 +1,59 @@
+<template>
+  <article id="recipe-info">
+    <div id="recipe-header">
+      <h3 @click="setCurrentRecipe(recipe)"
+          :class="{'active-title': currentRecipe === recipe}"
+      >{{ recipe.title }}</h3>
+      <i class="far fa-trash-alt"
+         title="delete recipe"
+         v-show="currentRecipe === recipe"
+         @click="deleteRecipe"></i>
+    </div>
+    <div v-show="currentRecipe === recipe">
+      <ul>
+        <li id="ingredient-list"
+            v-for="(ingredient, index) in recipe.ingredients"
+            :key="index">
+          {{ ingredient.ingredientName }} : {{ ingredient.ingredientQty }}
+          {{ ingredient.ingredientWeight }}
+        </li>
+      </ul>
+      <change-weekday></change-weekday>
+      <button id="update-button" @click="updateRecipe">Update recipe info</button>
+    </div>
+  </article>
+</template>
+
+<script>
+import {mapMutations, mapActions, mapState} from 'vuex'
+
+export default{
+  name: "RecipeInfo",
+  props: {
+    recipe: {
+      required: true
+    }
+    },
+  data() {
+    return {}
+  },
+  methods: {
+    ...mapMutations([
+      'removeDuplicates'
+    ]),
+    ...mapActions([
+      'deleteRecipe'
+    ]),
+    updateRecipe() {
+      this.$store.dispatch('setToCurrent')
+      this.$store.dispatch('updateRecipe')
+    },
+    setCurrentRecipe(recipe) {
+      this.$store.commit('setCurrentRecipe', recipe)
+    }
+  },
+  computed: mapState([
+    'recipes', 'currentRecipe', 'weekdays'
+  ])
+}
+</script>
