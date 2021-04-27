f20194019746a90d62f60ca20e41e576306669d2
Johan Åström <johan.astrom89@gmail.com>
2021-04-27 09:29:14 +0200

    Added form-data store module

---
+++ src/store/modules/form-data.js
@@ -0,0 +1,59 @@
+
+
+const state = {
+  title: '',
+  ingredientName: '',
+  ingredientQty: '',
+  ingredientWeight: '',
+  ingredients: [],
+  weekday: '',
+  image: '',
+  alt: ''
+}
+
+const mutations = {
+  setTitle(state, title) {
+    state.title = title
+  },
+  setIngredientName(state, ingredientName) {
+    state.ingredientName = ingredientName
+  },
+  setIngredientQty(state, ingredientQty) {
+    state.ingredientQty = ingredientQty
+  },
+  setIngredientWeight(state, ingredientWeight) {
+    state.ingredientWeight = ingredientWeight
+  },
+  setIngredients(state, ingredients) {
+    state.ingredients = ingredients
+  },
+  setWeekday(state, weekday) {
+    state.weekday = weekday
+  },
+  setImage(state, image) {
+    state.image = image
+  },
+  setAlt(state, alt) {
+    state.alt = alt
+  }
+}
+
+const actions = {
+  setToCurrent({commit, rootState}) {
+    let recipe = rootState.currentRecipe
+
+    commit('setTitle', recipe.title)
+    commit('setIngredients', recipe.ingredients.slice())
+    commit('setWeekday', recipe.weekday)
+    commit('setImage', recipe.image)
+    commit('setAlt', recipe.alt)
+  }
+}
+
+export default {
+  state,
+  mutations,
+  actions
+}
+
+
